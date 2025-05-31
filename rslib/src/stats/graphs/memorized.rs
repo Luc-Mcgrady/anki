190bb67cb57c683c56548263702990b7b3ff53d3
Luc Mcgrady <lucmcgrady@gmail.com>
2025-05-31 23:02:30 +0100

    Fix: Parameters none

--- rslib/src/stats/graphs/memorized.rs
+++ rslib/src/stats/graphs/memorized.rs
@@ -3,7 +3,7 @@ use crate::{
     scheduler::fsrs::memory_state::{self, fsrs_items_for_memory_states},
 };
 use anki_proto::stats::RevlogEntry;
-use fsrs::FSRS;
+use fsrs::{DEFAULT_PARAMETERS, FSRS};
 use itertools::{izip, Itertools};
 
 use super::{retention, GraphsContext};
@@ -31,15 +31,15 @@ pub fn convert_review_log_for_historical(log: RevlogEntry) -> HistoricalReviewLo
 
 impl GraphsContext {
     pub fn historical_fsrs(&self) -> Result<Vec<f32>> {
-        let fsrs = FSRS::new(None).unwrap();
-        let memory_states = fsrs_items_for_memory_states(
+        let fsrs = FSRS::new(Some(&DEFAULT_PARAMETERS)).unwrap();
+        let items = fsrs_items_for_memory_states(
             &fsrs,
             self.revlog.clone(),
             self.next_day_start,
             0.9,
             0.into(),
-        )?
-        .into_iter()
+        )?;
+        let memory_states = items
         .into_iter()
         .filter_map(|(_cid, item)| {
             item.as_ref().map(|item| {
@@ -50,27 +50,27 @@ impl GraphsContext {
             })
         })
         .collect_vec();
-
-        let mut retention = vec![0.; self.days_elapsed as usize];
-        for (revlogs, memory_states) in memory_states {
-            if let Ok(memory_states) = memory_states {
-                for (from_to, memory_state) in izip!(revlogs.windows(2), memory_states) {
-                    let start_day = from_to[0].days_elapsed(self.next_day_start) as usize;
-                    let end_day = from_to[1].days_elapsed(self.next_day_start) as usize;
-                    for (i, day) in retention
-                        .iter_mut()
-                        .take(end_day)
-                        .skip(start_day)
-                        .enumerate()
-                    {
-                        fsrs.current_retrievability(memory_state, i as u32, 0.2);
-                    }
+    
+    let mut retention = vec![0.; self.days_elapsed as usize];
+    for (revlogs, memory_states) in memory_states {
+        if let Ok(memory_states) = memory_states {
+            for (from_to, memory_state) in izip!(revlogs.windows(2), memory_states) {
+                let start_day = from_to[0].days_elapsed(self.next_day_start) as usize;
+                let end_day = from_to[1].days_elapsed(self.next_day_start) as usize;
+                for (i, day) in retention
+                .iter_mut()
+                .take(end_day)
+                .skip(start_day)
+                .enumerate()
+                {
+                    *day+=fsrs.current_retrievability(memory_state, i as u32, 0.2);
                 }
             }
+            }
         }
-
+        
         dbg!(&retention);
-
+        
         Ok(retention)
     }
 }
